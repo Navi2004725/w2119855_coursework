f99eeec1d7b0041dde3b6dc9ca01b342f951cd4e
Navi2004725 <naveeshabeydeera123@gmail.com>
2026-04-22 10:57:52 +0530

    3rd commit ✏️

--- smart-campus-api/src/main/java/config/ApplicationConfig.java
+++ smart-campus-api/src/main/java/config/ApplicationConfig.java
@@ -1,7 +1,11 @@
 package config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import filter.LoggingFilter;
 import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.ext.ContextResolver;
 import org.glassfish.jersey.server.ResourceConfig;
 
 @ApplicationPath("/api/v1")
@@ -9,5 +13,8 @@ public class ApplicationConfig extends ResourceConfig {
     public ApplicationConfig() {
         packages("resource", "mapper");
         register(LoggingFilter.class);
+        register((ContextResolver<ObjectMapper>) type -> new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS));
     }
 }
